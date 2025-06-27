0e249bfdbfd77f60f7b0879f751a5c81cdc966f6
manjunathdevagiri <manjunath.devagiri21@gmail.com>
2025-06-27 11:55:16 +0530

    GitMavenApp

---
+++ src/main/java/com/shanInfotech/gitMavenApp/App.java
@@ -0,0 +1,10 @@
+package com.shanInfotech.gitMavenApp;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
